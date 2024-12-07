4f1c7b5bdbe20f0651b6929b0f5c3d6725dab2b9
HussainALAradi5 <hussainaradi.ha@gmail.com>
2024-12-07 20:48:15 +0300

    add new item to the list

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ func test() {
 	taskItems := []string{taskOne, taskTwo, taskThree}
 	fmt.Println("Welcome to our todolist app")
 	printTasks(taskItems)
+	addTask(taskItems, "learn java")
 }
 func printTasks(taskItems []string) {
 
@@ -19,4 +20,9 @@ func printTasks(taskItems []string) {
 	for index, task := range taskItems {
 		fmt.Println("task#", index+1, task)
 	}
+}
+func addTask(taskItems []string, newTask string) {
+	update := append(taskItems, newTask)
+	fmt.Println("the updated list:")
+	printTasks(update)
 }
