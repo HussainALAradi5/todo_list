fb0db0eea62821bce500069acf55958a27a84e93
HussainALAradi5 <hussainaradi.ha@gmail.com>
2024-12-07 20:43:28 +0300

    declare functions to handle the print for the tasklists.

--- main.go
+++ main.go
@@ -11,6 +11,11 @@ func test() {
 	taskThree := "Learn Algorithms"
 	taskItems := []string{taskOne, taskTwo, taskThree}
 	fmt.Println("Welcome to our todolist app")
+	printTasks(taskItems)
+}
+func printTasks(taskItems []string) {
+
+	fmt.Println("List of Todos")
 	for index, task := range taskItems {
 		fmt.Println("task#", index+1, task)
 	}
