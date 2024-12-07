733bf1ff9a608a9f5f589b8124809874f0f9f134
HussainALAradi5 <hussainaradi.ha@gmail.com>
2024-12-07 20:27:12 +0300

    inilize the todolist

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	test()
+}
+func test() {
+	taskOne := "Learn go"
+	taskTwo := "Learn Fiber framework"
+	taskThree := "Learn Algorithms"
+	fmt.Println("Welcome to our todolist app")
+
+}
